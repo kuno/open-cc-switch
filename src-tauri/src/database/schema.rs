kuno/cc-switch

Add OpenWrt provider drawer drag reorder
Squash merge approved OpenWrt provider drawer drag reorder.\n\nKeeps drawer display order separate from failover queue priority via display_sort_index.

src-tauri/src/database/schema.rs
@@ -33,6 +33,7 @@ impl Database {
                 category TEXT,
                 created_at INTEGER,
                 sort_index INTEGER,
+                display_sort_index INTEGER,
                 notes TEXT,
                 icon TEXT,
                 icon_color TEXT,
@@ -339,6 +340,7 @@ impl Database {
             "in_failover_queue",
             "BOOLEAN NOT NULL DEFAULT 0",
         )?;
+        Self::add_column_if_missing(conn, "providers", "display_sort_index", "INTEGER")?;
 
         // 删除旧的 failover_queue 表（如果存在）
         let _ = conn.execute("DROP INDEX IF EXISTS idx_failover_queue_order", []);
@@ -443,6 +445,11 @@ impl Database {
                         Self::migrate_v9_to_v10(conn)?;
                         Self::set_user_version(conn, 10)?;
                     }
+                    10 => {
+                        log::info!("迁移数据库从 v10 到 v11（分离供应商展示排序与故障转移排序）");
+                        Self::migrate_v10_to_v11(conn)?;
+                        Self::set_user_version(conn, 11)?;
+                    }
                     _ => {
                         return Err(AppError::Database(format!(
                             "未知的数据库版本 {version}，无法迁移到 {SCHEMA_VERSION}"
@@ -474,6 +481,7 @@ impl Database {
         Self::add_column_if_missing(conn, "providers", "category", "TEXT")?;
         Self::add_column_if_missing(conn, "providers", "created_at", "INTEGER")?;
         Self::add_column_if_missing(conn, "providers", "sort_index", "INTEGER")?;
+        Self::add_column_if_missing(conn, "providers", "display_sort_index", "INTEGER")?;
         Self::add_column_if_missing(conn, "providers", "notes", "TEXT")?;
         Self::add_column_if_missing(conn, "providers", "icon", "TEXT")?;
         Self::add_column_if_missing(conn, "providers", "icon_color", "TEXT")?;
@@ -1220,6 +1228,24 @@ impl Database {
         log::info!("v9 -> v10 迁移完成：已添加 Hermes Agent 支持");
         Ok(())
     }
+
+    /// v10 -> v11 迁移：分离供应商展示排序与故障转移队列排序
+    fn migrate_v10_to_v11(conn: &Connection) -> Result<(), AppError> {
+        Self::add_column_if_missing(conn, "providers", "display_sort_index", "INTEGER")?;
+        if Self::has_column(conn, "providers", "sort_index")? {
+            conn.execute(
+                "UPDATE providers
+                 SET display_sort_index = sort_index
+                 WHERE display_sort_index IS NULL AND sort_index IS NOT NULL",
+                [],
+            )
+            .map_err(|e| AppError::Database(e.to_string()))?;
+        }
+
+        log::info!("v10 -> v11 迁移完成：已分离供应商展示排序");
+        Ok(())
+    }
+
     /// 插入默认模型定价数据
     /// 格式: (model_id, display_name, input, output, cache_read, cache_creation)
     /// 注意: model_id 使用短横线格式（如 claude-haiku-4-5），与 API 返回的模型名称标准化后一致

kuno/open-cc-switch

src-tauri/src/database/schema.rs
@@ -33,6 +33,7 @@ impl Database {
                 category TEXT,
                 created_at INTEGER,
                 sort_index INTEGER,
+                display_sort_index INTEGER,
                 notes TEXT,
                 icon TEXT,
                 icon_color TEXT,
@@ -418,6 +419,7 @@ impl Database {
             "in_failover_queue",
             "BOOLEAN NOT NULL DEFAULT 0",
         )?;
+        Self::add_column_if_missing(conn, "providers", "display_sort_index", "INTEGER")?;
 
         // 删除旧的 failover_queue 表（如果存在）
         let _ = conn.execute("DROP INDEX IF EXISTS idx_failover_queue_order", []);
@@ -561,6 +563,11 @@ impl Database {
                         log::info!("迁移数据库从 v17 到 v18（会话日志字节游标列）");
                         Self::migrate_v17_to_v18(conn)?;
                         Self::set_user_version(conn, 18)?;
+                    }
+                    18 => {
+                        log::info!("迁移数据库从 v18 到 v19（分离供应商展示排序与故障转移排序）");
+                        Self::migrate_v18_to_v19(conn)?;
+                        Self::set_user_version(conn, 19)?;
                     }
                     _ => {
                         return Err(AppError::Database(format!(
@@ -1619,6 +1626,23 @@ impl Database {
                 "INTEGER",
             )?;
         }
+        Ok(())
+    }
+
+    /// v18 -> v19: separate provider drawer display order from failover queue priority.
+    fn migrate_v18_to_v19(conn: &Connection) -> Result<(), AppError> {
+        Self::add_column_if_missing(conn, "providers", "display_sort_index", "INTEGER")?;
+        if Self::has_column(conn, "providers", "sort_index")? {
+            conn.execute(
+                "UPDATE providers
+                 SET display_sort_index = sort_index
+                 WHERE display_sort_index IS NULL AND sort_index IS NOT NULL",
+                [],
+            )
+            .map_err(|e| AppError::Database(e.to_string()))?;
+        }
+
+        log::info!("v18 -> v19 迁移完成：已分离供应商展示排序");
         Ok(())
     }
 